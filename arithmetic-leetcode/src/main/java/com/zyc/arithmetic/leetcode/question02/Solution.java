19c79af31bfdcbd1c120eac92fa67c4ab7116c7c
zhangyachao <1694706951@qq.com>
2021-09-22 13:43:50 +0800

    # 初始化仓库，提交工程

---
+++ arithmetic-leetcode/src/main/java/com/zyc/arithmetic/leetcode/question02/Solution.java
@@ -0,0 +1,16 @@
+package com.zyc.arithmetic.leetcode.question02;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        ListNode listNode1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+        System.out.println("[" + listNode1 + "]");
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        return null;
+    }
+
+}
---
+++ everyday-test/src/main/java/com/zyc/DayTest.java
@@ -0,0 +1,11 @@
+package com.zyc;
+
+public class DayTest {
+
+    public static void main(String[] args) {
+        String a = "/asdasdasd/";
+        String b = a.substring(0,a.length() - 1);
+        System.out.println(b);
+    }
+
+}
