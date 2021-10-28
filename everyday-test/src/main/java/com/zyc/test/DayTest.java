60f78030da672c82c6d702524e6777b7319be648
zhangyachao <1694706951@qq.com>
2021-10-28 16:59:56 +0800

    * 增加aop模块，正则工具类

--- everyday-test/src/main/java/com/zyc/test/DayTest.java
+++ everyday-test/src/main/java/com/zyc/test/DayTest.java
@@ -2,7 +2,10 @@ package com.zyc.test;
 
 import cn.hutool.log.Log;
 import com.google.common.collect.Lists;
+import com.zyc.utils.RegularExpression;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Comparator;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
@@ -12,7 +15,59 @@ public class DayTest {
     private static final Log log = Log.get();
     public static void main(String[] args) {
         DayTest dayTest = new DayTest();
-        dayTest.threadTest();
+        dayTest.regx("1.1");
+    }
+
+    private void regx(String str){
+        boolean matches = str.matches(RegularExpression.REGX_URL);
+        System.out.println(matches);
+    }
+
+    private void lambdaList(){
+        List<User> users = Lists.newArrayList();
+        users.add(new User(1,"aaa"));
+        users.add(new User(3,"aaa"));
+        users.add(new User(65,"aaa"));
+        users.add(new User(8,"aaa"));
+
+        users.forEach(user -> {
+            if(user.getId() == 3){
+                user.setName("ppp");
+            }
+        });
+        users.forEach(System.out::println);
+    }
+
+    private void checkUri(){
+        String address = "https://www.baidu.com/aaaad";
+        //URI uri = URI.create(address);
+        try {
+            URI uri = new URI(address);
+            System.out.println(uri.getPort());
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void doubleToInt(){
+        String str = "3000.0";
+        double v = Double.parseDouble(str);
+        Integer a = (int) v;
+        System.out.println(a);
+    }
+
+    private void listRemoveCp(){
+        List<String> lists = Lists.newArrayList();
+        lists.add("123");
+        lists.add("1234");
+        lists.add("1231");
+        lists.add("12343523");
+        Lists.newArrayList(lists).forEach(item -> {
+            if("1231".equals(item)){
+                lists.remove(item);
+            }
+        });
+        lists.forEach(System.out::println);
     }
 
     private void threadTest(){
---
+++ everyday-test/src/main/java/com/zyc/utils/RegularExpression.java
@@ -0,0 +1,19 @@
+package com.zyc.utils;
+
+public class RegularExpression {
+
+    /**
+     * url正则式
+     */
+    public final static String REGX_URL = "^((https|http|ftp|rtsp|mms)?://)"  //https、http、ftp、rtsp、mms
+            + "?(([0-9a-z_!~*'().&=+$%-]+: )?[0-9a-z_!~*'().&=+$%-]+@)?" //ftp的user@
+            + "(([0-9]{1,3}\\.){3}[0-9]{1,3}" // IP形式的URL- 例如：199.194.52.184
+            + "|" // 允许IP和DOMAIN(域名)
+            + "([0-9a-z_!~*'()-]+\\.)*" // 域名- www.
+            + "([0-9a-z][0-9a-z-]{0,61})?[0-9a-z]\\." // 二级域名
+            + "[a-z]{2,6})" // first level domain- .com or .museum
+            + "(:[0-9]{1,5})?" // 端口号最大为65535,5位数
+            + "((/?)|" // a slash isn't required if there is no file name
+            + "(/[0-9a-z_!~*'().;?:@&=+$,%#-]+)+/?)$";
+
+}
